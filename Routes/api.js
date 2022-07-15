687df655411368c5c70e52f44f721bd5fece5e08
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-07-15 05:37:11 +0530

    cleaned the api file

--- Routes/api.js
+++ Routes/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json('ILM API');
+    res.json('SpringDB API');
 });
 // add middleware to jobController
 // router.use('/job' , AuthRequired('User') ,  JobController);
