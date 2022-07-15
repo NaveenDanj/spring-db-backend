5316ff1039867f42cb6585de848a2bbba9c824bd
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-07-16 04:29:06 +0530

    added add app endpoint

--- Controllers/app.controller.js
+++ Controllers/app.controller.js
@@ -5,26 +5,63 @@ const router = express.Router();
 const Joi = require('../Validator/index');
 
 
-router.post('/add' , (req , res) =>{
+router.post('/add' , async(req , res) =>{
 
     let validator = Joi.object({
-        app_name: Joi.string().required().min(6).max(255),
+        app_name: Joi.string().required().min(3).max(255),
         app_api_key: Joi.string().required().min(6).max(255),
-        app_description: Joi.string().required().min(6).max(255)
+        workspace_id : Joi.number().required()
     });
 
     try {
 
-        const val = validator.validateAsync(req.body, {
+        const val = await validator.validateAsync(req.body, {
             abortEarly: false
         });
 
+        // check if workspace is valid and have access to the user
+        const workspace = await db.workspaces.findOne({
+            where: {
+                id: val.workspace_id
+            }
+        });
+
+        if(!workspace){
+            return res.status(400).json({
+                message: "Workspace not found"
+            });
+        }
+
+        const workspaceUser = await db.workspace_users.findOne({
+            where: {
+                workspace_id: val.workspace_id,
+                user_id: req.user.user.id
+            }
+        });
+
+
+
+        if(!workspaceUser){
+            return res.status(400).json({
+                message: "You don't have access to this workspace"
+            });
+        }
+
+        console.log('data' , workspaceUser.dataValues);
+
+        if(workspaceUser.dataValues.role != 'admin' && workspaceUser.dataValues.role != 'owner'){
+            return res.status(400).json({
+                message: "You don't have access to this workspace"
+            });
+        }
+
+        // create the app
         let app = await db.app.create({
             name: val.app_name,
-            api_key: val.app_api_key,
-            description: val.app_description,
-            workspace_id : 1
+            app_api_key : val.app_api_key,
+            workspace_id : val.workspace_id
         });
+
 
         return res.status(200).json({
             message: "App added successfully",
@@ -39,3 +76,5 @@ router.post('/add' , (req , res) =>{
 
 
 });
+
+module.exports = router;
--- Routes/api.js
+++ Routes/api.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const AuthRequired = require('../Middlewares/AuthRequired');
+
 const authController = require('../Controllers/Auth/auth.controller');
+const appController = require( '../Controllers/app.controller');
 
 router.get('/', (req, res) => {
     res.json('SpringDB API');
@@ -10,5 +13,6 @@ router.get('/', (req, res) => {
 // router.use('/job' , AuthRequired('User') ,  JobController);
 
 router.use('/auth' , authController);
+router.use('/app' , AuthRequired('user') , appController);
 
 module.exports = router;
