ae90720f6349914702da6f63d22d27236b6d4c3c
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-07-16 01:26:57 +0530

    added generate app token method

--- Services/token.service.js
+++ Services/token.service.js
@@ -1,10 +1,15 @@
 const jwt = require('jsonwebtoken');
+const Uuid = require('uuid');
 
 module.exports = {
 
     generateToken(email){
         // expire in 30 days
         return jwt.sign( {email : email} , process.env.JWT_SECRET , {expiresIn: '30d'});
+    },
+
+    generateAppToken(){
+        return Uuid.v4();
     }
 
 
