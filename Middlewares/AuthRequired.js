655253a1144f4027cb5470c9c94e778c1ed87b75
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-07-15 18:34:51 +0530

    added auth required middleware

--- Middlewares/AuthRequired.js
+++ Middlewares/AuthRequired.js
@@ -6,42 +6,47 @@ const AuthRequired = (userRole) => {
     return async (req , res , next) => {
 
         const token = req.headers['authorization'];
-
-        let roleMapper = {
-            'Admin' : 2,
-            'User' : 1
-        }
         
         if(!token){
-            return res.status(401).send({error: 'Token not provided'})
+            return res.status(401).send({error: 'Unauthenticated'});
         }
 
         // in future check if token is in blacklist(logged out before token expired)
         try{
-            let checkExists = await AccessToken.findOne({token : token});
+
+            let checkExists = await db.access_token.findOne({
+                where: {
+                    token: token
+                }
+            });
             
             if(!checkExists){
-                return res.status(401).send({error: 'Token not valid'})
+                return res.status(401).send({error: 'Unauthenticated'})
             }
 
             if(checkExists.blocked){
-                return res.status(401).send({error: 'Token expired!'})
+                return res.status(401).send({error: 'Unauthenticated'})
             }
 
         }catch(err){
-            return res.status(401).send({error: 'Token not valid'})
+            return res.status(401).send({error: 'Unauthenticated'})
         }
         
 
         jwt.verify(token, process.env.JWT_SECRET, async(err, userObject) => {
         
-            if (err) return res.status(403).json({message: 'Invalid token'});
+            if (err) return res.status(403).json({message: 'Unauthenticated'});
         
             try{
                 let user = await User.findOne({email : userObject.email}).select('-password');
-                if( roleMapper[user.role] < roleMapper[userRole] ){
-                    return res.status(403).send({error: 'You are not authorized'})
+
+                if(!user){
+                    return res.status(403).json({message: 'Unauthenticated'});
                 }
+            
+                // if( roleMapper[user.role] < roleMapper[userRole] ){
+                //     return res.status(403).send({error: 'You are not authorized'})
+                // }
 
                 req.user = user;
                 next();
