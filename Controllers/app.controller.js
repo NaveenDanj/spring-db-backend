9199248904ded5f2c563e63d1f14410dd8223ad0
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-07-16 06:57:17 +0530

    added add users to app endpoint

--- Controllers/app.controller.js
+++ Controllers/app.controller.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const db = require("../Database");
-const AuthRequired = require("../Middlewares/AuthRequired");
 const router = express.Router();
 const Joi = require('../Validator/index');
 
@@ -47,7 +46,6 @@ router.post('/add' , async(req , res) =>{
             });
         }
 
-        console.log('data' , workspaceUser.dataValues);
 
         if(workspaceUser.dataValues.role != 'admin' && workspaceUser.dataValues.role != 'owner'){
             return res.status(400).json({
@@ -74,6 +72,105 @@ router.post('/add' , async(req , res) =>{
         });
     }
 
+});
+
+
+router.post('/add-users-to-app' , async(req , res) => {
+
+    let validator = Joi.object({
+        user_ids: Joi.array().items(Joi.number()).required(),
+        app_id: Joi.number().required()
+    });
+
+    try{
+
+        let val = await validator.validateAsync(req.body, {
+            abortEarly: false
+        });
+        
+        // check if user id's are valid
+        for(let i = 0; i < val.user_ids.length; i++ ){
+
+            let user = await db.users.findOne({
+                where: {
+                    id: val.user_ids[i]
+                }
+            });
+
+            if(!user){
+                return res.status(400).json({
+                    message: "User not found"
+                });
+            }
+
+        }
+
+        // check if app id is valid
+        let app = await db.app.findOne({
+            where: {
+                id: val.app_id
+            }
+        });
+
+        if(!app){
+            return res.status(400).json({
+                message: "App not found"
+            });
+        }
+
+        // check if app is owned by the user
+        let workspace_users = await db.workspace_users.findOne({
+            where: {
+                workspace_id: app.dataValues.workspace_id,
+                user_id: req.user.user.id
+            }
+        });
+        
+        if(!workspace_users){
+            return res.status(400).json({
+                message: "You don't have access to this workspace"
+            });
+        }
+
+        if(workspace_users.dataValues.role != 'admin' && workspace_users.dataValues.role != 'owner'){
+            return res.status(400).json({
+                message: "You don't have access to this workspace"
+            });
+        }
+
+        // add users to app
+        for(let i = 0; i < val.user_ids.length; i++ ){
+
+            // check if user is already added to the app
+            let app_user = await db.app_users.findOne({
+                where: {
+                    app_id: val.app_id,
+                    user_id: val.user_ids[i]
+                }
+            });
+
+            if(!app_user){
+                let user = await db.app_users.create({
+                    app_id: val.app_id,
+                    user_id: val.user_ids[i]
+                });
+            }
+
+
+        }
+
+
+        return res.status(200).json({
+            message: "Users added to app successfully"
+        });
+
+    }catch(err){
+
+        return res.status(400).json({
+            message: err.message
+        });
+
+    }
 
 });
 
